4ac5c380e66359c3e666d0817d19865426d73c0d
Teja Linux <raviteja.yella@gmail.com>
2021-08-23 19:04:49 +0530

    FOREACH method for iterables

--- Fundamentals-1/loops.js
+++ Fundamentals-1/loops.js
@@ -81,5 +81,49 @@ let count = 0;
 while (dice !== 6){
     dice = Math.floor(Math.random() * 7);
     count ++;
+    if (dice == 6) console.log("Congrats! Checking dice value again:" ,dice,"You got it in", count, "counts");
 }
-console.log("Congrats! Checking dice value again:" ,dice,"You got it in", count, "counts");
+console.log("while loop ended");
+
+
+let checkArray = [];
+// randArray.forEach()
+randArray.forEach( (element) => {
+    console.log("This is getting looped");
+    checkArray.push(element);
+    return checkArray;
+});
+console.log(checkArray);
+
+
+// another challenge
+
+function printForecast(array){
+    let finalString = "...";
+    for (let i = 0; i < array.length; i++){
+        finalString += `${array[i]} degrees C in ${i + 1} days...`
+    }
+    return finalString;
+}
+
+let arr = [17, 21, 23];
+console.log(printForecast(arr));
+
+
+for (let i = randArray.length - 1; i >= 0; i--){
+    if (typeof randArray[i] === 'boolean') break;
+    randArray.pop();
+}
+
+console.log(randArray);
+
+
+let anotherArray = [];
+let indexer = randArray.length;
+randArray.forEach(function (element){
+    indexer --;
+    anotherArray[indexer] = element;
+})
+console.log(randArray);
+console.log(anotherArray);
+
