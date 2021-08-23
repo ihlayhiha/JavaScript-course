d330beebc0e1e2d5f6ad6245f052127ed3c902cb
Teja Linux <raviteja.yella@gmail.com>
2021-08-23 12:11:13 +0530

    express and declare functions, try catch method, generating new errors  and some other keywords like instanceof

--- Fundamentals-1/ifElseStatements.js
+++ Fundamentals-1/ifElseStatements.js
@@ -81,3 +81,91 @@ console.log(n); // 10
 
 
 // Challenge
+
+console.log("The 10th fibonacci value is : ", factorial(10));
+
+function factorial(n){
+    if (n === 0) return 0
+    else if (n === 1) return 1
+    return (factorial(n - 1) + factorial(n - 2))
+}
+
+
+
+let fib = (n)=> {
+    if (n == 0) return 0
+    else if (n === 1) return 1
+    return fib(n - 1) + fib(n - 2)
+}
+console.log(fib(10));
+
+const randArrowFunction = (a, b, c) => a + b + c;
+console.log(randArrowFunction(1, 2, 3));
+
+
+let declareFunction = function (a, b, c) { return a + b + c}
+
+console.log(declareFunction(1, 2, 3));
+
+declareFunction = 100;
+fib = 200;
+console.log(declareFunction);
+console.log(fib);
+factorial = 23;
+console.log(factorial);
+
+// console.log(factorial(200));      wont work because factorial is not a function anymore
+
+const fibonacci = function (a){
+    if (a === 0) return 0;
+    if (a === 1) return 1;
+    return fibonacci(a - 1) + fibonacci(a - 2);
+}
+
+console.log(fibonacci(10));
+
+const expressFactorial = function (a){
+    if (Math.floor(a) != a || typeof(a) != "number"){
+        throw new Error("Invalid input for a factorial function. Input has to be a non-negative integer");
+    }
+    if (a === 0) return 1;
+    if (a === 1) return 1;
+    return a * expressFactorial(a - 1);
+}
+
+
+try {
+    expressFactorial(10.2);
+} catch (error) {
+    console.log("yeah, you can only have  factorials for non negative integers.");
+}
+
+// console.log(expressFactorial(10.2));
+
+// let randCheck = 10.4;
+// console.log(Math.floor(randCheck));
+
+// if (Math.abs(randCheck) !== randCheck)
+//     console.log("randCheck is not integer");
+
+try{
+    expressFactorial("ten");
+}
+catch (error){
+    console.log("Factorial function is only for non negative integers");
+}
+
+try{
+    expressFactorial('yeah');
+}
+catch (e){
+    if (e instanceof TypeError) console.log("Yeah this is just Error");
+    // console.log("Factorial function is only for non negative integers");
+    else console.log("Yeah this is some other Error");
+}
+finally{
+    console.log(expressFactorial(10));
+}
+
+randCheck = new String();
+console.log(randCheck instanceof String);
