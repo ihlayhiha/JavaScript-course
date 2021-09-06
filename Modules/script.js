87ec9d319b3db9eab746aa846f7afc181c97aa65
Teja Linux <raviteja.yella@gmail.com>
2021-09-06 10:57:49 +0530

    Using NPM packages

--- Modules/script.js
+++ Modules/script.js
@@ -3,6 +3,7 @@ import './shoppingCart.js';     // Is executed before any code in the rest of th
 import { addToCart, totalPrice, totalQuantity, randName as myName, tax } from './shoppingCart.js';
 import * as ShoppingCart from './shoppingCart.js';  // importing whole module as a module object
 import add, { cart } from './shoppingCart.js';        // how to import default exports
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
 
 console.log("Importing module");
 
@@ -23,3 +24,25 @@ add('cookies', 200);
 // if we mutate values here, the values change there as well, and vice-versa 
 console.log(ShoppingCart.cart);
 console.log(cart);
+
+const state = {
+    cart : [
+        {product : "Banana", quantity : 10},
+        {product : "Apple",   quantity : 20},
+    ],
+
+    user : { loggedIn : true},
+};
+
+
+console.log(state);
+
+const stateClone = Object.assign({}, state);    // creating a shallow clone
+const stateDeepClone = cloneDeep(state);
+// cloneDeep from lodash makes a deep clone that doesn't mutate when u mutate the original object
+
+state.user.loggedIn = false;
+
+console.log(stateClone.user.loggedIn);
+console.log(stateDeepClone.user.loggedIn);
+
